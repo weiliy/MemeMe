dccc2dac40a8b7de8a0d6588e38ba9c6c63faa5f
Weili Y <yi@weiliy.net>
2024-01-05 15:59:52 +0800

    feat: add pickup image

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePickView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pickAImageBtn(_ sender: Any) {
+        let pickImageController = UIImagePickerController()
+        pickImageController.delegate = self
+        present(pickImageController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        print(info)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        print("user cancelled")
+        dismiss(animated: true, completion: nil)
+    }
 }
 
